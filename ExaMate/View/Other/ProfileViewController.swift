e37f091f254ce4d2ff308de782ca9b2c8b9d337a
Ümit Şimşek <umitsimsek002@gmail.com>
2024-05-08 13:03:44 +0300

    create user profile page & fetch user info

---
+++ ExaMate/View/Other/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  ExaMate
+//
+//  Created by Ümit Şimşek on 8.05.2024.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ExaMate/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewModel.swift
+//  ExaMate
+//
+//  Created by Ümit Şimşek on 8.05.2024.
+//
+
+import Foundation

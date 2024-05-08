af3c63d1dae223eae9cafe8504a0636fa43e4d6f
Ümit Şimşek <umitsimsek002@gmail.com>
2024-05-08 12:21:05 +0300

    photo viewer page created

---
+++ ExaMate/View/Other/PhotoViewerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoViewerViewController.swift
+//  ExaMate
+//
+//  Created by Ümit Şimşek on 8.05.2024.
+//
+
+import UIKit
+
+class PhotoViewerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

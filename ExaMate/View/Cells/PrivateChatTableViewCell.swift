cc2c902f0aa639b55336806714914c6f7ca095af
Ümit Şimşek <umitsimsek002@gmail.com>
2024-07-30 13:32:25 +0300

    listenin for new conversations

---
+++ ExaMate/View/Cells/PrivateChatTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PrivateChatTableViewCell.swift
+//  ExaMate
+//
+//  Created by Ümit Şimşek on 30.07.2024.
+//
+
+import UIKit
+
+class PrivateChatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ExaMate/ViewModel/PrivateChatTableViewCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PrivateChatTableViewCellViewModel.swift
+//  ExaMate
+//
+//  Created by Ümit Şimşek on 30.07.2024.
+//
+
+import Foundation
